47857c3b5736bef6cade07c10704e894e40c2fc6
Paolo Bianchessi <Paolo.bianchessi05@gmail.com>
2021-10-30 18:46:53 +0200

    .

--- Informatica/C/Es17(pari_dispari)/mcd.c
+++ Informatica/C/Es17(pari_dispari)/mcd.c
@@ -1,23 +1,28 @@
 /*
     Autore: Paolo Bianchessi 3ic
     Data: 29/10/2021
-    Descrizione: 
+    Descrizione:
 */
 
 #include <stdio.h>
-
+int mcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return mcd(b, a % b);
+    }
+}
 int main()
 {
-    int a, b, r, div;
-    printf("Inserisci un numero a: ");
+    int a, b;
+    printf("Inserisci un numero: ");
     scanf("%d", &a);
-    printf("Inserisci un numero b: ");
+    printf("inserisci un altro numero: ");
     scanf("%d", &b);
-    while (r != 0)
-    {
-        div = a / b;
-        r = a % b;
-    }
 
-    return 0;
+    printf("il massimo comun divisore e': %d", mcd(a, b));
 }

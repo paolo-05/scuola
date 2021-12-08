9721aa4756af16960d73bbef5e40ff9be2dfd836
paolo-05 <Paolo.bianchessi05@gmail.com>
2021-12-08 20:29:53 +0100

    .

---
+++ Informatica/C/es_bisestile_date/date.c
@@ -0,0 +1,80 @@
+/*
+    Autore: Paolo Bianchessi 3ic
+    Data: 07/12/2021
+    Descrizione: 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int bisestile(int anno)
+{
+    if (anno % 4 == 0)
+    {
+        if (anno % 100 == 0)
+        {
+            if (anno % 400 == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int controllaData(int gg, int mm, int aa)
+{
+    return gg + mm * 12 + aa * 365;
+}
+
+int main()
+{
+    int g1, m1, a1, bisestile1;
+    int g2, m2, a2, bisestile2;
+    printf("Inserisci una data (gg/mm/aa/): ");
+    scanf("%d/%d/%d", &g1, &m1, &a1);
+
+    printf("Inserisci un'altra data (gg/mm/aa): ");
+    scanf("%d/%d/%d", &g2, &m2, &a2);
+    bisestile1 = bisestile(a1);
+    if (bisestile1)
+    {
+        printf("\nIl primo anno e' bisestile\n");
+    }
+    else
+    {
+        printf("Il primo anno non e' bisestile\n");
+    }
+    bisestile2 = bisestile(a2);
+    if (bisestile2)
+    {
+        printf("Il secondo anno e' bisestile\n");
+    }
+    else
+    {
+        printf("Il secondo anno non e' bisestile\n");
+    }
+    g1 = controllaData(g1, m1, a1);
+    g2 = controllaData(g2, m2, a2);
+    if (g1 > g2)
+    {
+        printf("\nLa prima data e' maggiore della seconda");
+    }
+    else
+    {
+        printf("\nLa seconda data è maggiore della prima\n");
+    }
+
+    return 0;
+}
--- Informatica/C/es_dieci_bisestile/anno.c
+++ Informatica/C/es_dieci_bisestile/anno.c
@@ -6,25 +6,32 @@
 
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int anno;
     printf("Inserisci un anno: ");
-    scanf("%d",&anno);
+    scanf("%d", &anno);
 
-    if (anno%4==0){
-        if (anno%100==0){
-            if (anno%400==0){
-                printf("\nL'anno %d e' bisestile",anno);
+    if (anno % 4 == 0)
+    {
+        if (anno % 100 == 0)
+        {
+            if (anno % 400 == 0)
+            {
+                printf("\nL'anno %d e' bisestile", anno);
             }
-            else{
-                printf("\nL'anno %d e' non bisestile",anno);
+            else
+            {
+                printf("\nL'anno %d e' non bisestile", anno);
             }
         }
-        else{
-            printf("\nL'anno %d e' bisestile",anno);
+        else
+        {
+            printf("\nL'anno %d e' bisestile", anno);
         }
     }
-    else{
-        printf("\nL'anno %d e' non bisestile",anno);
+    else
+    {
+        printf("\nL'anno %d e' non bisestile", anno);
     }
 }

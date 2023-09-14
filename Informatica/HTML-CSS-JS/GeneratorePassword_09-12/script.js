53f51989d38c8afc7064dc18029aeac324f1736a
Paolo Bianchessi <Paolo.bianchessi05@gmail.com>
2023-09-14 20:42:10 +0200

    .

--- Informatica/HTML-CSS-JS/GeneratorePassword_09-12/script.js
+++ Informatica/HTML-CSS-JS/GeneratorePassword_09-12/script.js
@@ -1,33 +1,67 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slider = document.getElementById('password-length');
+    const lowerCheckbox = document.getElementById('lower');
+    const upperCheckbox = document.getElementById('upper');
+    const numbersCheckbox = document.getElementById('numbers');
+    const symbolsCheckbox = document.getElementById('symbols');
+
+    const savedPasswordLength = localStorage.getItem('passwordLength');
+    if (savedPasswordLength !== null) {
+        slider.value = savedPasswordLength;
+        document.getElementById('password-length-value').textContent = savedPasswordLength;
+    }
+
+    const savedLowerChecked = localStorage.getItem('lowerChecked') === 'true';
+    lowerCheckbox.checked = savedLowerChecked;
+
+    const savedUpperChecked = localStorage.getItem('upperChecked') === 'true';
+    upperCheckbox.checked = savedUpperChecked;
+
+    const savedNumbersChecked = localStorage.getItem('numbersChecked') === 'true';
+    numbersCheckbox.checked = savedNumbersChecked;
+
+    const savedSymbolsChecked = localStorage.getItem('symbolsChecked') === 'true';
+    symbolsCheckbox.checked = savedSymbolsChecked;
+});
+
 const slider = document.getElementById('password-length');
-slider.addEventListener("input", function(){
+slider.addEventListener("input", function () {
     const sliderValue = slider.value;
     const displayValue = document.getElementById('password-length-value');
     displayValue.textContent = sliderValue;
+    localStorage.setItem('passwordLength', sliderValue);
+});
+
+const checkboxes = document.querySelectorAll('.checkbox');
+checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("change", function () {
+        localStorage.setItem(checkbox.id + 'Checked', checkbox.checked);
+    });
 });
 
 const button = document.getElementById('generate-button');
-button.addEventListener("click", function(){
-    const lowerChecked = document.getElementById('lower-case').checked;
-    const upperChecked = document.getElementById('upper-case').checked;
+button.addEventListener("click", function () {
+    const lowerChecked = document.getElementById('lower').checked;
+    const upperChecked = document.getElementById('upper').checked;
     const numbersChecked = document.getElementById('numbers').checked;
     const symbolsChecked = document.getElementById('symbols').checked;
 
     let typesEnabled = [];
 
-    if(!lowerChecked && !upperChecked && !numbersChecked && !symbolsChecked){
+    if (!lowerChecked && !upperChecked && !numbersChecked && !symbolsChecked) {
         alert("Seleziona un'opzione!");
         return;
     }
-    if(lowerChecked){
+    if (lowerChecked) {
         typesEnabled.push("lower");
     }
-    if(upperChecked){
+    if (upperChecked) {
         typesEnabled.push("upper");
     }
-    if(numbersChecked){
+    if (numbersChecked) {
         typesEnabled.push("numbers");
     }
-    if(symbolsChecked){
+    if (symbolsChecked) {
         typesEnabled.push("symbols");
     }
 
@@ -52,8 +86,19 @@ button.addEventListener("click", function(){
         }
     }
 
-    passwordDisplay = document.getElementById('password-display');
-    passwordDisplay.textContent = password;
+    const passwordDisplay = document.getElementById('password-display');
+    passwordDisplay.textContent = "";
+
+    let currentIndex = 0;
+
+    const animationInterval = setInterval(function () {
+        if (currentIndex < password.length) {
+            passwordDisplay.textContent += password[currentIndex];
+            currentIndex++;
+        } else {
+            clearInterval(animationInterval);
+        }
+    }, 100);
 });
 
 function upperChar(){

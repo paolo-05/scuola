94851239fecf3087d2c249a5226c5f32de15f65c
Paolo Bianchessi <paolo.bianchessi05@gmail.com>
2023-11-05 11:37:58 +0100

    .

--- Informatica/Verifiche/06-10/scripts/budget.js
+++ Informatica/Verifiche/06-10/scripts/budget.js
@@ -1,39 +1,39 @@
-const description = document.getElementById('desc');
-const qt = document.getElementById('qt');
-const select = document.getElementById('select');
+const description = document.getElementById("desc");
+const qt = document.getElementById("qt");
+const select = document.getElementById("select");
 
-const addButton = document.getElementById('add');
-const resetButton = document.getElementById('reset');
+const addButton = document.getElementById("add");
+const resetButton = document.getElementById("reset");
 
-const list = document.getElementById('list');
-const totSpan = document.getElementById('saldo');
+const list = document.getElementById("list");
+const totSpan = document.getElementById("saldo");
 let tot = 0;
 
-addButton.addEventListener('click', () => {   
-    if(qt.value == ''){
-        alert("Inserisci un importo valido");
-        return;
-    }
-    if(description.value == ''){
-        alert("Inserisci una descrizione valida");
-        return;
-    }
-    list.innerHTML += `${description.value}: ${select.value} ${Number(qt.value)} Euro <br>`;
-    
-    if(select.value === '+')
-    {
-        tot += Number(qt.value);
-    }
-    else {
-        tot -= Number(qt.value);
-    }
-    
-    totSpan.textContent = ""+tot;
+addButton.addEventListener("click", () => {
+  if (qt.value == "") {
+    alert("Inserisci un importo valido");
+    return;
+  }
+  if (description.value == "") {
+    alert("Inserisci una descrizione valida");
+    return;
+  }
+  list.innerHTML += `${description.value}: ${select.value} ${Number(
+    qt.value
+  )} Euro <br>`;
+
+  if (select.value === "+") {
+    tot += Number(qt.value);
+  } else {
+    tot -= Number(qt.value);
+  }
+
+  totSpan.textContent = "" + tot;
 });
 
-resetButton.addEventListener('click', () => {
-    qt.value = '';
-    description.value = '';
-    list.textContent = '';
-    saldo.textContent = '';
+resetButton.addEventListener("click", () => {
+  qt.value = "";
+  description.value = "";
+  list.textContent = "";
+  saldo.textContent = "";
 });

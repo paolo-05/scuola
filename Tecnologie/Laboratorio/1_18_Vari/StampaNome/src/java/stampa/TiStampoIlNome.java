1bb29d8ecfd43e5eda7438143df4a06a9ab8f910
Paolo Bianchessi <paolo.bianchessi05@gmail.com>
2024-01-18 20:37:56 +0100

    .

---
+++ Tecnologie/Laboratorio/1_18_Vari/StampaNome/src/java/stampa/TiStampoIlNome.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
+ */
+package stampa;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author paolo
+ */
+@WebService(serviceName = "TiStampoIlNome")
+public class TiStampoIlNome {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
